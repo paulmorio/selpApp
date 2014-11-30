d830efb22b57f08d0c2e3ca64ec43260e03cf849
Paul Scherer <s1206798@pavia.inf.ed.ac.uk>
2014-11-30 16:43:27 +0000

    included the project web application real_fantasy_adventure_app as part of the applications that the webserver will run with the rfa_website

--- rfa_website/rfa_website/settings.py
+++ rfa_website/rfa_website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'real_fantasy_adventure_app',
 )
 
 MIDDLEWARE_CLASSES = (
