7b912853f9c2f583186839412193bfcac43b82ae
Paul Scherer <s1206798@vicenza.inf.ed.ac.uk>
2014-12-01 11:33:59 +0000

    updated development database, included files necessary for the web app to work on django with some initial models of avatars/userprofiles.

--- rfa_website/real_fantasy_adventure_app/models.py
+++ rfa_website/real_fantasy_adventure_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Avatar model
+class Avatar(models.Model):
+	"""The Avatar is the User's character within the game"""
+	username = models.ForeignKey("auth.User")
+	level = models.IntegerField(default=1)
+	created_date = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.username
+		
+# # Quest model
+# class Quest(models.Model):
+# 	"""docstring for Quest"""
+# 	title = models.CharField(max_length=256)
+# 	description = models.TextField()
+# 	created_date = models.DateTimeField(default=timezone.now)
+# 	min_Avatar_Level_Requirement = models.IntegerField(default=1)
+# 	rewardPoints = 
+
+
+# 	def __init__(self, arg):
+# 		super(Quest, self).__init__()
+# 		self.arg = arg
+
+# 	def __str__(self):
+# 		return self.title
+		
+
+
+# # Points Model
+# class Point(models.Model):
+# 	"""docstring for Point"""
+# 	def __init__(self, arg):
+# 		super(Point, self).__init__()
+# 		self.arg = arg
