b813f62200d22f929e4ea6cdc7292e2ab49c6076
Paul Scherer <s1206798@stork.inf.ed.ac.uk>
2014-12-02 17:12:33 +0000

    Added Quest Model to app models file with its respective queryset, registered this on the admin file, added app to settings.

--- rfa_website/real_fantasy_adventure_app/admin.py
+++ rfa_website/real_fantasy_adventure_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+
+class QuestAdmin(admin.ModelAdmin):
+	list_display = ("title", "created_date")
+	prepopulated_fields = {"slug": ("title",)} #automatically populates the slug with JavaScript in admin
 
 # Register your models here.
+admin.site.register(models.Quest, QuestAdmin)
--- rfa_website/real_fantasy_adventure_app/models.py
+++ rfa_website/real_fantasy_adventure_app/models.py
@@ -1,41 +1,53 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+#Avatar model
+# class Avatar(models.Model):
+# 	"""The Avatar is the User's character/profile within the game"""
+# 	realUser = models.ForeignKey(User)
 
-# Avatar model
-class Avatar(models.Model):
-	"""The Avatar is the User's character/profile within the game"""
-	user = models.ForeignKey("auth.User")
-	username = CharField(max_length=80)
-	level = models.IntegerField(default=1)
-	created_date = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.username
-		
-# # Quest model
-# class Quest(models.Model):
-# 	"""docstring for Quest"""
-# 	title = models.CharField(max_length=256)
-# 	description = models.TextField()
-# 	created_date = models.DateTimeField(default=timezone.now)
-# 	min_Avatar_Level_Requirement = models.IntegerField(default=1)
-# 	rewardPoints = 
-
-
-# 	def __init__(self, arg):
-# 		super(Quest, self).__init__()
-# 		self.arg = arg
+# 	biography = models.TextField(blank=True)
+# 	level = models.IntegerField(default=1)
+# 	created_date = models.DateTimeField(dauto_now_add=True)
+# 	modified_date = models.DateTimeField(auto_now=True)
 
 # 	def __str__(self):
-# 		return self.title
-		
+# 		return __str__(realUser)
 
+class QuestQuerySet(models.QuerySet):
+	def published(self):
+		return self.filter(publish=True)
 
-# # Points Model
-# class Point(models.Model):
-# 	"""docstring for Point"""
-# 	def __init__(self, arg):
-# 		super(Point, self).__init__()
-# 		self.arg = arg
+# Quest model
+class Quest(models.Model):
+	"""docstring for Quest"""
+	title = models.CharField(max_length=256)
+	description = models.TextField()
+	publish = models.BooleanField(default=False)
+
+	# Point Categories
+	num_professional_points = models.IntegerField(default=0)
+	num_athletic_points = models.IntegerField(default=0)
+	num_academic_points = models.IntegerField(default=0)
+
+	# URL Friendly Reference
+	slug = models.SlugField(max_length=200, unique=True)
+
+	#Date Attributes
+	created_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+
+	# Connection to Queryset for admin tasks
+	objects = QuestQuerySet.as_manager()
+
+	# ONCE AVATAR HAS BEEN Makde
+	#min_Avatar_Level_Requirement = models.IntegerField(default=1)
+	#rewardPoints = 
+
+	def __str__(self):
+		return self.title
+	
+	class Meta:
+		verbose_name = "Quest Entry"
+		verbose_name_plural = "Quest Entries"
+		ordering = ["-created_date"]
