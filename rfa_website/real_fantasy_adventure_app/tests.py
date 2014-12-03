32f76f8f9192bfb1c561b636cbdfbb4b96d8ae07
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-03 10:59:26 +0000

    Wrote test case for quest creation and publishing with reference to QuestQuerySet as well, tests can be run via ./manage.py test

--- rfa_website/real_fantasy_adventure_app/tests.py
+++ rfa_website/real_fantasy_adventure_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from . models import Quest
 
-# Create your tests here.
+# TestCases for Quest
+class QuestEntryTest(TestCase):
+
+	def test_create_unpublished(self):
+		quest = Quest(title="Test Title", description="Test Description", publish=False)
+		quest.save()
+		self.assertEqual(Quest.objects.all().count(), 1)
+		self.assertEqual(Quest.objects.published().count(), 0)
+		quest.publish = True
+		quest.save()
+		self.assertEqual(Quest.objects.published().count(), 1)
