852979c6fa6a77009332191c84a7a22c8d78eb29
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-03 10:49:17 +0000

    Started Preparing for creation of user page, and viewing of quests.

--- rfa_website/real_fantasy_adventure_app/views.py
+++ rfa_website/real_fantasy_adventure_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import generic
+from . import models
 # Create your views here.
+
+# class QuestIndex(generic.ListView):
+# 	queryset = models.Entry.objects.published()
+# 	template = "quests.html"
+# 	paginate_by = 5
--- rfa_website/rfa_website/urls.py
+++ rfa_website/rfa_website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include("django_markdown.urls")),
+    #url(r'^', include("quest.urls")),
 )
