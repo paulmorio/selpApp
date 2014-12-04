24bf10a3a569e55af0bbe52472f9a6f1d9f8755a
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-04 22:30:09 +0000

    Removed templates folder from the directory containing the application so that it may be placed under project directory and all templates for the website are stored there under different subdirectories for each application. This is for the purposes of design decoupling and portability of the application.
    Urls and views have been updated to show a simple text,
    url mappings have been decoupled. Several files have been changed
    but has simplified structure and implements encouraged architecture
    pattern for django projects.

--- rfa_website/real_fantasy_adventure_app/views.py
+++ rfa_website/real_fantasy_adventure_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 from . import models
@@ -7,3 +8,7 @@ from . import models
 # 	queryset = models.Entry.objects.published()
 # 	template = "quests.html"
 # 	paginate_by = 5
+
+def index(request):
+	return HttpResponse("Welcome Hero, this is just a test site, your princess is in another castle")
+
--- rfa_website/rfa_website/urls.py
+++ rfa_website/rfa_website/urls.py
@@ -7,7 +7,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'rfa_website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Enables Admin
     url(r'^admin/', include(admin.site.urls)),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Enables Markdown inclusion
     url(r'^markdown/', include("django_markdown.urls")),
-    #url(r'^', include("quest.urls")),
+
+    # Let URL mapping of anything related to the app be handled by urls.py in the app, this decouples
+    # the mappings and makes it possible to move real_fantasy_adventure_app between projects.
+    url(r'^real_fantasy_adventure_app/', include('real_fantasy_adventure_app.urls')),
 )
