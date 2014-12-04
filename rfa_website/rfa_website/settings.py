a9fc97f405b8e91936f0f82eb15079f938cb4bac
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-04 23:13:59 +0000

    Added path variables and pointed one to templates directory so that the paths to the directory contianing the templates is not hardcoded, this has caused difficulty before

--- rfa_website/rfa_website/settings.py
+++ rfa_website/rfa_website/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Directory where the settings are
+SETTINGS_DIR = os.path.dirname(__file__)
+
+# Finding the Path to Project directory
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -78,8 +84,17 @@ USE_L10N = True
 
 USE_TZ = False
 
+# Path to templates
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
+# Template files directory
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
