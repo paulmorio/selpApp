789479f33e54a49f21ceeff5d69b516168c64548
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-05 01:41:53 +0000

    added ignoring of .pyc (python byte code) files in repo (will need to run removal command of all ignored files in the end), while updating models to include avatars

--- rfa_website/real_fantasy_adventure_app/models.py
+++ rfa_website/real_fantasy_adventure_app/models.py
@@ -13,7 +13,52 @@ from django.utils import timezone
 
 # 	def __str__(self):
 # 		return __str__(realUser)
+#####################################
+######         AVATARS         ######
+#####################################
+class AvatarQuerySet(models.QuerySet):
+	def confirmed(self):
+		return self.filter(confirm=True)
 
+# Avatar Model
+class Avatar(object):
+	"""docstring for Avatar"""
+	# General
+	name = models.CharField(max_length=200)
+	bio = models.TextField()
+
+	# Level (important to access Quests)
+	level = models.IntegerField(default=1)
+
+	# Point Categories
+	num_professional_points = models.IntegerField(default=0)
+	num_athletic_points = models.IntegerField(default=0)
+	num_academic_points = models.IntegerField(default=0)
+
+	# Date Related Attributes
+	created_date = models.DateTimeField(auto_now_add=True)
+	modified_date = models.DateTimeField(auto_now=True)
+
+	# URL Friendly Reference (for use with profile pages)
+	slug = models.SlugField(max_length=200, unique=True)
+
+	# Connection to its queryset for admin tasks
+	objects = AvatarQuerySet.as_manager()
+
+	def __str__(self):
+		return self.name
+
+	def get_total_points():
+		return num_professional_points + num_academic_points + num_athletic_points
+
+	class Meta:
+		verbose_name = "Avatar Entry"
+		verbose_name_plural = "Avatar Entries"
+		ordering = ["-level"]
+
+#####################################
+######         QUESTS          ######
+#####################################
 class QuestQuerySet(models.QuerySet):
 	def published(self):
 		return self.filter(publish=True)
