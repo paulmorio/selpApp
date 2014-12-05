97579d3d12935d798f251200461d67ee6180c870
Paul Scherer <s1206798@avellino.inf.ed.ac.uk>
2014-12-05 00:00:22 +0000

    Changed views to serve the html templates, as well as wirting the index html file itself, added image to use next in the html

--- rfa_website/real_fantasy_adventure_app/views.py
+++ rfa_website/real_fantasy_adventure_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views import generic
+from django.template import RequestContext
 from . import models
 # Create your views here.
 
@@ -9,6 +11,20 @@ from . import models
 # 	template = "quests.html"
 # 	paginate_by = 5
 
-def index(request):
-	return HttpResponse("Welcome Hero, this is just a test site, your princess is in another castle")
+# Old index view handler
+# def index(request):
+# 	return HttpResponse("Welcome Hero, this is just a test site, your princess is in another castle")
 
+def index(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('real_fantasy_adventure_app/index.html', context_dict, context)
