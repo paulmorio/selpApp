8597e5fa53d08b11868c08baa9cd15d1830929cd
Paul Scherer <s1206798@vervcelli.inf.ed.ac.uk>
2014-12-11 15:49:24 +0000

    One small step for man... A giant leap for me, using the django templating language and fixing the request --> template rendering in the views.py file the index page now shows top avatars based on the points they garnered in their respective fields.

--- rfa_website/real_fantasy_adventure_app/views.py
+++ rfa_website/real_fantasy_adventure_app/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render_to_response
 from django.views import generic
-from django.template import RequestContext
-from . import models
+from real_fantasy_adventure_app.models import Quest, Avatar, MyQuest
+
 # Create your views here.
 
 # class QuestIndex(generic.ListView):
@@ -16,29 +15,23 @@ from . import models
 # 	return HttpResponse("Welcome Hero, this is just a test site, your princess is in another castle")
 
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
-    context = RequestContext(request)
 
     # make a list of the top 5 players by points
-    avatars_by_academic = models.Avatar.objects.order_by('-num_academic_points')[:5]
-    #avatars_by_professional = models.Avatar.objects.order_by('-num_professional_points')[:5]
-    #avatars_by_athletic = models.Avatar.objects.order_by('-num_athletic_points')[:5]
+    avatars_by_academic = Avatar.objects.order_by('-num_academic_points')[:5]
+    avatars_by_professional = Avatar.objects.order_by('-num_professional_points')[:5]
+    avatars_by_athletic = Avatar.objects.order_by('-num_athletic_points')[:5]
 
     # make a context dictionary for each
-    context_dict_academic = {'avatars': avatars_by_academic}
-    #context_dict_professional = {'avatars': avatars_by_professional}
-    #context_dict_athletic = {'avatars': avatars_by_athletic}
+    context_dict = {'academics': avatars_by_academic, 'professionals': avatars_by_professional, 'athletes': avatars_by_athletic}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('real_fantasy_adventure_app/index.html', context_dict_academic, context)
+    return render(request, 'real_fantasy_adventure_app/index.html', context_dict)
 
 def about(request):
-	context = RequestContext(request)
 	context_dict = {'boldthing' : "You are an adventurer"}
 
-	return render_to_response('real_fantasy_adventure_app/about.html', context_dict, context)
+	return render(request, 'real_fantasy_adventure_app/about.html', context_dict)
 
 
