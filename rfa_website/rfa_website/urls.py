9a88adfeb2667bab200baf9f9d2fc369352b04db
Paul Scherer <s1206798@daae.inf.ed.ac.uk>
2014-12-17 17:38:05 +0000

    Deleted an unneeded URL mapping of a feature I have decided not to implement

--- rfa_website/rfa_website/urls.py
+++ rfa_website/rfa_website/urls.py
@@ -10,13 +10,11 @@ urlpatterns = patterns('',
     # Enables Admin
     url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Enables Markdown inclusion
+    # Enables Markdown inclusion in the admin page, used for dev_blog app 
+    # (if time permits after rfa is complete)
     url(r'^markdown/', include("django_markdown.urls")),
 
-    # Let URL mapping of anything related to the app be handled by urls.py in the app, this decouples
+    # Let URL mapping of anything related to the rfa app be handled by urls.py in the app, this decouples
     # the mappings and makes it possible to move real_fantasy_adventure_app between projects.
     url(r'^real_fantasy_adventure_app/', include('real_fantasy_adventure_app.urls')),
 )
