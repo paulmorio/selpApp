d5df46201a1e3254481ad1836c2260580987618d
Paul Scherer <s1206798@grasmere.inf.ed.ac.uk>
2014-12-17 04:28:21 +0000

    added slug line creation check to model tests

--- rfa_website/real_fantasy_adventure_app/tests.py
+++ rfa_website/real_fantasy_adventure_app/tests.py
@@ -2,11 +2,17 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from . models import Quest, Avatar, MyQuest
 from django.contrib.auth.models import User
+from . statChange import inc_professional_points, inc_athletic_points, inc_academic_points
 
-# TestCases for Quest Entries
+# TestCases for Quest Models
 class QuestEntryTest(TestCase):
 
 	def test_create_unpublished(self):
+		"""
+		Creates a Quest instance that is unconfirmed, makes sure the 
+		queryset doesnt find this, confirms the instance, and tries 
+		the search again
+		"""
 		quest = Quest(title="Test Title", description="Test Description", publish=False)
 		quest.save()
 		self.assertEqual(Quest.objects.all().count(), 1)
@@ -15,10 +21,14 @@ class QuestEntryTest(TestCase):
 		quest.save()
 		self.assertEqual(Quest.objects.published().count(), 1)
 
-# TestCases for Avatar Entries
+# TestCases for Avatar Models
 class AvatarEntryTest(TestCase):
 
 	def test_create_unconfirmed(self):
+		"""
+		Creates a Avatar instance that is unconfirmed, makes sure the 
+		queryset doesnt find this, confirms the instance, and tries the search again
+		"""
 		testUser = User.objects.create_user(username = "testUser", email="test@test.com", password="test1234")
 		avatar = Avatar(user=testUser, nickname="TestName", bio="Test Biography", confirm=False)
 		avatar.save()
@@ -28,10 +38,25 @@ class AvatarEntryTest(TestCase):
 		avatar.save()
 		self.assertEqual(Avatar.objects.confirmed().count(), 1)
 
-# TestCases for MyQuest Entries
+	def test_slug_line_creation(self):
+		"""
+		tests the proper creation of slugs depending on the nickname of the avatar 
+		(ie. Random String --> random-string)
+		"""
+		testUser = User.objects.create_user(username = "testUser", email="test@test.com", password="test1234")
+		avatar = Avatar(user=testUser, nickname="Random String", bio="Test Biography", confirm=False)
+		avatar.save()
+		self.assertEqual(avatar.slug, 'random-string')
+
+# TestCases for MyQuest Models
 class MyQuestEntryTest(TestCase):
 	
 	def test_create_unpublished(self):
+		"""
+		Creates a MyQuest instance that is unconfirmed, makes sure the 
+		queryset doesnt find this, confirms the instance, and tries 
+		the search again
+		"""
 		testUser = User.objects.create_user(username = "testUser", email="test@test.com", password="test1234")
 		testAvatar = Avatar(user=testUser, nickname="TestName", bio="Test Biography", confirm=True)
 		testAvatar.save()
@@ -42,3 +67,16 @@ class MyQuestEntryTest(TestCase):
 		myQuest.publish = True
 		myQuest.save()
 		self.assertEqual(MyQuest.objects.published().count(), 1)
+
+	def test_slug_line_creation(self):
+		"""
+		tests the proper creation of slugs depending on the title of the myQuest 
+		(ie. Random String --> random-string)
+		"""
+		testUser = User.objects.create_user(username = "testUser", email="test@test.com", password="test1234")
+		testAvatar = Avatar(user=testUser, nickname="TestName", bio="Test Biography", confirm=True)
+		testAvatar.save()
+		myQuest = MyQuest(avatar=testAvatar, title="Random String", description="Test Description", publish=False)
+		myQuest.save()
+		self.assertEqual(myQuest.slug, 'random-string')
+
